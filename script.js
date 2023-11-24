0c9bc833d2ce3502fea548fb33ff3ccd81de7fe9
0mBarde <ombarde73@gmail.com>
2023-11-24 23:18:37 +0530

    commited

---
+++ script.js
@@ -0,0 +1,12 @@
+// {
+// $(document).ready(function () {
+//     $("#events-slider").owlCarousel({
+//         items: 1, // Display one event box at a time
+//         loop: true,
+//         margin: 20, // Adjust the margin to match your CSS
+//         nav: true, // Show navigation arrows
+//         autoplay: true, // Enable auto-play
+//         autoplayTimeout: 5000, // Set the auto-play interval in milliseconds (5 seconds in this example)
+//     });
+// });
+// }
